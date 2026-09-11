2a54636612cfffe01b9bbd51ce87cf3333f7e574
BuiHoaBku97 <vanhoabku97@gmail.com>
2026-09-12 00:14:05 +0700

    CircuitBreaker

--- pharmacy-service/src/main/java/startup/vn/pharmacyservice/controllers/PharmacyController.java
+++ pharmacy-service/src/main/java/startup/vn/pharmacyservice/controllers/PharmacyController.java
@@ -3,8 +3,10 @@ package startup.vn.pharmacyservice.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import startup.vn.pharmacyservice.clients.WarehouseClientService;
 
 @RestController
 @RequestMapping("/api/v1/pharmacies")
@@ -17,8 +19,16 @@ public class PharmacyController {
     @Value("${app.hotline}")
     private String hotline;
 
+    private final WarehouseClientService warehouseClientService;
+
     @GetMapping("/hello")
     public String hello() {
         return String.format("Hello from %s Pharmacy! For assistance, call us at %s.", branchName, hotline);
+    }
+
+    // Example endpoint that calls the warehouse service with circuit breaker
+    @GetMapping("/stock/{productId}")
+    public String checkStock(@PathVariable String productId) {
+        return warehouseClientService.checkStock(productId);
     }
 }
